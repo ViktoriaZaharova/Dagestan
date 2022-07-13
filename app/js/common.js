c632ca5a7188f0c942699b81aff288322860ff8a
Viktoria <zaharova181114@gmail.com>
2022-07-13 22:11:05 +0300

    Initial commit

---
+++ app/js/common.js
@@ -0,0 +1,5 @@
+$('.reviews-slider').slick({
+    slidesToShow: 3,
+    prevArrow: '<button type="button" class="slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"></button>'
+});
