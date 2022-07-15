59c21d4027beefa09fb5cdf5aa99480430c2d0b3
Viktoria <zaharova181114@gmail.com>
2022-07-15 13:41:17 +0300

    768 version

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,14 @@
 $('.reviews-slider').slick({
     slidesToShow: 3,
     prevArrow: '<button type="button" class="slick-prev"></button>',
-    nextArrow: '<button type="button" class="slick-next"></button>'
+    nextArrow: '<button type="button" class="slick-next"></button>',
+    variableWidth: true,
+    responsive: [
+        {
+            breakpoint: 1360,
+            settings: {
+                centerMode: true
+            }
+        }
+    ]
 });
